65e1929a5f8c91f1acce83358a155205d7671a2c
Angelo Galanti <angelo.galanti@gmail.com>
2025-02-21 15:08:47 +0100

    Refactor app.py for improved readability and consistency in code style

--- src/e16/app.py
+++ src/e16/app.py
@@ -1,27 +1,17 @@
 import json
-import re
 import sqlite3
-from flask import (
-    Flask,
-    g,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    session,
-    flash,
-    jsonify,
-)
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, g, jsonify, render_template
 import os
+
 
 app = Flask(__name__)
 app.secret_key = "your-secret-key-here"
 
-with open('config.json') as config_file:
+with open("config.json") as config_file:
     config = json.load(config_file)
-    DATABASE = config['DATABASE']
-    INITIALIZATION = config['INITIALIZATION']
+    DATABASE = config["DATABASE"]
+    INITIALIZATION = config["INITIALIZATION"]
+
 
 def get_db():
     """Connessione al database"""
@@ -30,19 +20,21 @@ def get_db():
         g.db.row_factory = sqlite3.Row
     return g.db
 
+
 def init_db():
     """Inizializza il database"""
     with app.app_context():
         db = get_db()
         # Load schema from queries folder
         with app.open_resource(INITIALIZATION) as f:
-            db.executescript(f.read().decode('utf8'))
+            db.executescript(f.read().decode("utf8"))
         db.commit()
+
 
 @app.teardown_appcontext
 def close_db(error):
     """Chiude la connessione al database"""
-    if hasattr(g, 'db'):
+    if hasattr(g, "db"):
         g.db.close()
 
 
@@ -51,9 +43,10 @@ def raw_squadre():
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM SQUADRA")
-    entries = cursor.fetchall()
+    entries: list[sqlite3.Row] = cursor.fetchall()
     cursor.close()
     return jsonify([dict(row) for row in entries])
+
 
 @app.route("/squadre")
 def squadre():
@@ -66,7 +59,7 @@ def squadre():
 
 
 if __name__ == "__main__":
-    # check if the file DATABASE exists
+    # check if the file database exists
     if not os.path.exists(DATABASE):
         init_db()
     app.run(debug=True, port=50849)
